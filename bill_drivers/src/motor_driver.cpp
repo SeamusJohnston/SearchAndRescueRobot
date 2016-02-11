96c842308808ea22263e87595000bec046cceb87
Seamus Johnston <seamusbjohnston@gmail.com>
2016-02-11 16:31:10 +0000

    Functional PI and encoder fixes

--- bill_drivers/src/motor_driver.cpp
+++ bill_drivers/src/motor_driver.cpp
@@ -43,22 +43,22 @@ void drive(const int left_cmd, const int right_cmd)
 
     if (right_cmd >= 0)
     {
-        digitalWrite(MOTORA_FORWARD, LOW);
-    }
-    else
-    {
-        digitalWrite(MOTORA_FORWARD, HIGH);
-    }
-    if (left_cmd >= 0)
-    {
         digitalWrite(MOTORB_FORWARD, LOW);
     }
     else
     {
         digitalWrite(MOTORB_FORWARD, HIGH);
     }
-    softPwmWrite(MOTORA_PWM, std::abs(right_cmd));
-    softPwmWrite(MOTORB_PWM, std::abs(left_cmd));
+    if (left_cmd >= 0)
+    {
+        digitalWrite(MOTORA_FORWARD, LOW);
+    }
+    else
+    {
+        digitalWrite(MOTORA_FORWARD, HIGH);
+    }
+    softPwmWrite(MOTORA_PWM, std::abs(left_cmd));
+    softPwmWrite(MOTORB_PWM, std::abs(right_cmd));
 
 }
 
@@ -99,7 +99,7 @@ void drivePI(int heading, float dt)
     }
 
     float heading_command = heading_error * KP_DRIVE + heading_error_drive_sum * KI_DRIVE;
-
+    ROS_INFO("Heading: %i, Com Heading: %i, heading_error: %i, heading_command: %f", heading, last_command_msg.heading, heading_error, heading_command);
     // Note: this PI calculation assumes forward motion, since the robot should never have to reverse
     // Except for construction check, but the errors will be 0 for said check
 
