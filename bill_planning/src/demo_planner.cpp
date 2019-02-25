a72c637c2dbb18300b8f650a25a79f85e813e0f0
Seamus Johnston <seamusbjohnston@gmail.com>
2019-02-25 18:02:16 -0500

    Working demo planner

--- bill_planning/src/demo_planner.cpp
+++ bill_planning/src/demo_planner.cpp
@@ -31,6 +31,7 @@ struct StateMachine{
     {
         switch (state)
         {
+            ROS_INFO("Advance state, current state: %i", state);
             case 0:
             {
                 publishStop();
@@ -62,12 +63,14 @@ struct StateMachine{
 
     void runMachine()
     {
+        ROS_INFO("State machine running.");
         status = machineStatus::RUNNING;
         timer.start();
     }
 
     void pauseMachine()
     {
+        ROS_INFO("State machine stopped.");
         status = machineStatus::STOPPED;
         timer.stop();
     }
@@ -78,12 +81,13 @@ ros::Publisher fan_pub;
 bill_msgs::MotorCommands command_msg;
 StateMachine state_machine;
 int StateMachine::state = 0;
-std::mutex mutex;
+//std::mutex mutex;
 int current_heading = 0;
 
 void publishStop()
 {
-    std::lock_guard<std::mutex> lk(mutex);
+    //std::lock_guard<std::mutex> lk(mutex);
+    ROS_INFO("Commanding stop");
     command_msg.command = bill_msgs::MotorCommands::STOP;
     command_msg.heading = 0;
     command_msg.speed = 0;
@@ -92,7 +96,8 @@ void publishStop()
 
 void publishDrive(const int heading, const float speed)
 {
-    std::lock_guard<std::mutex> lk(mutex);
+    //std::lock_guard<std::mutex> lk(mutex);
+    ROS_INFO("Commanding drive, heading: %i and speed: %f", heading, speed);
     command_msg.command = bill_msgs::MotorCommands::DRIVE;
     command_msg.heading = heading;
     command_msg.speed = speed;
@@ -101,7 +106,8 @@ void publishDrive(const int heading, const float speed)
 
 void publishTurn(const int heading)
 {
-    std::lock_guard<std::mutex> lk(mutex);
+    //std::lock_guard<std::mutex> lk(mutex);
+    ROS_INFO("Commanding turn, heading: %i", heading);
     command_msg.command = bill_msgs::MotorCommands::TURN;
     command_msg.heading = heading;
     command_msg.speed = 0; // Speed is hardcoded in the motor driver for turning
@@ -118,7 +124,7 @@ void fireCallback(const std_msgs::Bool::ConstPtr& msg)
 {
     if (msg->data)
     {
-        std::lock_guard<std::mutex> lk(mutex);
+        //std::lock_guard<std::mutex> lk(mutex);
         state_machine.pauseMachine();
         publishStop();
 
@@ -137,14 +143,14 @@ void ultrasonicCallback(const std_msgs::Float32::ConstPtr& msg)
 {
     if (msg->data <= ULTRA_TRIGGER_DIST)
     {
-        std::lock_guard<std::mutex> lk(mutex);
+        //std::lock_guard<std::mutex> lk(mutex);
         state_machine.pauseMachine();
         publishTurn((current_heading + 10) % 360); // Request rotation by 10 degrees clockwise
         // Since IMU isn't operational, the heading published does nothing but set the direction
         // Therefore we have to start a short timer to actually represent the 10 degree turn
         ros::Duration(0.5).sleep();
-	// TODO: Current heading should update from IMU once working, so remove this when it does
-	current_heading += 10;
+	    // TODO: Current heading should update from IMU once working, so remove this when it does
+	    current_heading += 10;
         publishStop();
         state_machine.runMachine();
     }
@@ -157,7 +163,7 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     ros::Subscriber sub_odom = nh.subscribe("fused_odometry", 1, fusedOdometryCallback);
     ros::Subscriber sub_fire = nh.subscribe("fire", 1, fireCallback);
-    ros::Subscriber sub_ultrasonic = nh.subscribe("ultrasonic", 10, ultrasonicCallback);
+    ros::Subscriber sub_ultrasonic = nh.subscribe("ultrasonic", 1, ultrasonicCallback);
     motor_pub = nh.advertise<bill_msgs::MotorCommands>("motor_cmd", 100);
     fan_pub = nh.advertise<std_msgs::Bool>("fan", 100);
 
