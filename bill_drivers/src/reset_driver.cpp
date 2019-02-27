89aafe1656e938954c76534bdb8f1203e547b8be
Seamus Johnston <seamusbjohnston@gmail.com>
2019-02-26 19:01:21 -0500

    Fixed encoder driver, and cleaned and formatted code.

--- bill_drivers/src/reset_driver.cpp
+++ bill_drivers/src/reset_driver.cpp
@@ -6,21 +6,21 @@
 
 void setup()
 {
-	// Init GPIO 
+    // Init GPIO
     wiringPiSetupGpio();
-	
-	pinMode(BUTTON_PIN, INPUT);
-	pullUpDnControl(BUTTON_PIN, PUD_UP);
-	ROS_INFO("Reset pin is: %i, and set up to PULL UP", BUTTON_PIN);
+
+    pinMode(BUTTON_PIN, INPUT);
+    pullUpDnControl(BUTTON_PIN, PUD_UP);
+    ROS_INFO("Reset pin is: %i, and set up to PULL UP", BUTTON_PIN);
 }
 
 std_msgs::Bool read()
 {
     // Create message to publish
     std_msgs::Bool msg;
-	
+
     // Populate the message
-	msg.data = (digitalRead(BUTTON_PIN) == LOW);
+    msg.data = (digitalRead(BUTTON_PIN) == LOW);
 
     return msg;
 }
@@ -47,9 +47,9 @@ int main(int argc, char** argv)
         {
             // Publish message, and spin thread
             reset_pub.publish(msg);
-			
-			ROS_INFO("Reset Button Status: %s", msg.data ? "Pressed" : "Not Pressed");
-			
+
+            ROS_INFO("Reset Button Status: %s", msg.data ? "Pressed" : "Not Pressed");
+
             ros::spinOnce();
         }
         past_reset.data = msg.data;
