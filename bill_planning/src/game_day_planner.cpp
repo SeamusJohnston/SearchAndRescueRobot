6de411f4d29dc6fb04cafa26370f892b1c706000
osboxes.org <jiwooo.jang@gmail.com>
2019-03-11 10:50:33 -0400

    Refactored DriveToPoint to accomdate the queue of target points better and for use with GridSearch

--- bill_planning/src/game_day_planner.cpp
+++ bill_planning/src/game_day_planner.cpp
@@ -35,11 +35,14 @@ void fusedOdometryCallback(const nav_msgs::Odometry::ConstPtr& msg)
     int currentX = (int)std::trunc(msg->pose.pose.position.x);
     int currentY = (int)std::trunc(msg->pose.pose.position.y);
 
+    SensorReadings::current_x_tile = currentX;
+    SensorReadings::current_Y_tile = currentY;
+
     // This may cause weird behaviour when the robot is on the edges of a tile
     if (SensorReadings::currentTargetPoint.x == currentX && SensorReadings::currentTargetPoint.y == currentY)
     {
         // We have arrived at our current target point
-        planner.ProcessNextDrivePoint(SensorReadings::currentTargetPoint.x, SensorReadings::currentTargetPoint.y);
+        planner.ProcessNextDrivePoint(currentX, currentY);
     }
 }
 
